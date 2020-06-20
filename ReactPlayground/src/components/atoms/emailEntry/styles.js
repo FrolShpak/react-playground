d7b99f53f95acca6a29b24254298f9344680bb9e
Frol Shpak <frol.shpak@gmail.com>
2020-06-20 19:18:57 -0400

    Playing with styles

---
+++ ReactPlayground/src/components/atoms/emailEntry/styles.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native'
+import { baseStyles } from '_styles'
+  
+ export const styles = StyleSheet.create({  
+   emailEntry: {      
+     ...baseStyles.textInput
+   }
+ })
--- ReactPlayground/src/styles/index.js
+++ ReactPlayground/src/styles/index.js
@@ -1,6 +1,13 @@
+import { StyleSheet } from 'react-native'
 import * as Colors from './colors';
 import * as Spacing from './spacing';
 import * as Typography from './typography';
 import * as Mixins from './mixins';
 
 export { Typography, Spacing, Colors, Mixins };
+
+export const baseStyles = StyleSheet.create({
+    textInput:{
+        ...Mixins.margin(10, 10, 10, 10)
+    }
+});
